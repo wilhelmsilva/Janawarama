769c8824692334eb089371273176ebe3c34fcde4
bawanthathilan <bawantharathnayaka@gmail.com>
2020-07-18 17:11:40 +0530

    get parties

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 //import { stat } from 'fs'
 
 import axios from 'axios'
+import { isTemplateElement } from '@babel/types';
 
 
 
@@ -11,10 +12,12 @@ import axios from 'axios'
 export default class hero extends Component {
   constructor(props){
     super(props)
-    this.state = {districtdata : []}  
+    this.state = {
+      districtdata : [],
+      parties:[]}
   }
   componentDidMount(){
-    debugger
+    
     axios.get('https://janawarama.helakuru.lk/api/election/district',{headers:{'api-key': '0d9e15c7-3189-4402-8e3f-e5073ff00281'}})
     .then(response =>{
       console.log(response)
@@ -24,14 +27,26 @@ export default class hero extends Component {
       
     })
     .catch(error => console.log(error.response));
+
+    axios.get('https://janawarama.helakuru.lk/api/election/parties',{headers:{'api-key': '0d9e15c7-3189-4402-8e3f-e5073ff00281'}})
+    .then(response =>{
+      console.log(response)
+      this.setState({
+        parties:response.data.data
+      });
+      
+    })
+    .catch(error => console.log(error.response));
   }
+
+  
     render() {
 
         return (
               <div>
               <div class="jumbotron jumbotron-fluid">
                 <div class="container">
-                  <h1 class="display-4">චන්ද දායකයන් පිලිබද නිවැරදි තොරතුරු දැනගනිමු</h1>
+                  <h1 class="display-4">චන්ද අපේක්ශකයන් පිලිබද නිවැරදි තොරතුරු දැනගනිමු</h1>
                   <p class="lead">Powered By Janawarama API</p>
                 </div>
               </div>
@@ -49,16 +64,28 @@ export default class hero extends Component {
                     </button>
                     
                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                      {this.state.districtdata.map(data =>(<a className='dropdown-item' href="#" value={data.D_NAME}>{data.D_NAME}</a>))}
-                      
+                      {this.state.districtdata.map(data =>(<a className='dropdown-item'  href="#" value={data.D_NAME}>{data.D_NAME}</a>))}
                     </div>
+                    
                   </div>
 
                   
                   
                   </div>
                   <div class="col-sm">
-                  
+                  <div class="dropdown">
+                    <button class="btn btn-warning dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down-right-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-9.5 3h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 0-1 0v2.793L5.854 5.146a.5.5 0 1 0-.708.708L9.293 10H6.5a.5.5 0 0 0 0 1z"/>
+                    </svg> &nbsp;
+                    පක්ශය තොරන්න
+                    </button>
+                    
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      {this.state.parties.map(data =>(<a key='party' className='dropdown-item' href="#" value={data.PARTY_NAME}>{data.PARTY_NAME}</a>))}
+                    </div>
+                    
+                  </div>
                   </div>
                   <div class="col-sm">
                   
