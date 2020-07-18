d24f00f7cc77ee75996a8922b5e941ac1cb5191f
bawanthathilan <bawantharathnayaka@gmail.com>
2020-07-19 00:01:44 +0530

    Updates

---
+++ precache-manifest.9d83444df639a6b2e22c00269e3e4648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b756d7ca9627db1383ebb6c28c892acc",
+    "url": "/Janawarama/index.html"
+  },
+  {
+    "revision": "da8e9729d2a7a81b7e4d",
+    "url": "/Janawarama/static/css/main.73650ad2.chunk.css"
+  },
+  {
+    "revision": "1624c6b8c898e750f43e",
+    "url": "/Janawarama/static/js/2.d09b0c27.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Janawarama/static/js/2.d09b0c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8e9729d2a7a81b7e4d",
+    "url": "/Janawarama/static/js/main.1f9e961c.chunk.js"
+  },
+  {
+    "revision": "61a7b6c94a44e050c1a6",
+    "url": "/Janawarama/static/js/runtime-main.560204ba.js"
+  }
+]);
